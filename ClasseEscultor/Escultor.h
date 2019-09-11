8ee80e34e01154afb94c35fef4b39419d7ce4183
matheusslr <54174040+matheusslr@users.noreply.github.com>
2019-09-11 08:16:34 -0300

    Update Escultor.h

--- ClasseEscultor/Escultor.h
+++ ClasseEscultor/Escultor.h
@@ -10,17 +10,17 @@ private:
     float r,g,b,a;  //cores
 
 public:
-    Escultor(Voxel ***v, int _nx, int _ny, int _nz);
+    Escultor(int _nx, int _ny, int _nz);
     ~Escultor();
-    void setColor(Voxel ***v, float r, float g, float b, float alpha);
-    void putVoxel(Voxel ***v, int x, int y, int z);
-    void cutVoxel(Voxel ***v, int x, int y, int z);
-    void putBox(Voxel ***v, int x0, int x1, int y0, int y1, int z0, int z1);
-    void cutBox(Voxel ***v, int x0, int x1, int y0, int y1, int z0, int z1);
-    void putEsfera(Voxel ***v, int xc, int yc, int zc, int raio);
-    void cutEsfera(Voxel ***v, int xc, int yc, int zc, int raio);
-    void putElipse(Voxel ***v, int xc, int yc, int zc, int rx, int ry, int rz);
-    void cutElipse(Voxel ***v, int xc, int yc, int zc, int rx, int ry, int rz);
+    void setColor(float r, float g, float b, float alpha);
+    void putVoxel(int x, int y, int z);
+    void cutVoxel(int x, int y, int z);
+    void putBox(int x0, int x1, int y0, int y1, int z0, int z1);
+    void cutBox(int x0, int x1, int y0, int y1, int z0, int z1);
+    void putEsfera(int xc, int yc, int zc, int raio);
+    void cutEsfera(int xc, int yc, int zc, int raio);
+    void putElipse(int xc, int yc, int zc, int rx, int ry, int rz);
+    void cutElipse(int xc, int yc, int zc, int rx, int ry, int rz);
     void writeOFF(string teste);
 
 };
