166e7834260218f2e10fa4204957e086db0c85e9
davisouzaf <davi_sfernandes@hotmail.com>
2019-09-11 14:09:47 -0300

    fiz um comeco

---
+++ 3dSculptor/Sculptor.cpp
@@ -0,0 +1,6 @@
+#include "escultor.h"
+
+Escultor::Escultor()
+{
+
+}
---
+++ 3dSculptor/Sculptor.h
@@ -0,0 +1,11 @@
+#ifndef ESCULTOR_H
+#define ESCULTOR_H
+
+
+class Escultor
+{
+public:
+    Escultor();
+};
+
+#endif // ESCULTOR_H
