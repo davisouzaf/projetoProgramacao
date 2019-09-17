4e1742159c4dd3fc1c0df2704a9cce64dcbdd597
davisouzaf <davi_sfernandes@hotmail.com>
2019-09-17 11:25:45 -0300

    faltando so o destrutor, corrigir a elipse e rever a alocaçao dinamica

--- 3dSculptor/main.cpp
+++ 3dSculptor/main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){
     Sculptor s(16,16,16);
     s.setColor(1,1,0,1);
-    s.putBox(1,5,3,6,4,5);
-    s.cutBox(1,2,3,4,4,4);
-    s.writeOFF("testebox1.off");
+    s.putBox(1,16,1,16,1,16);
+    s.cutBox(5,10,10,15,1,5);
+    s.writeOFF("testebox3.off");
     cout << "Hello World!" << endl;
     return 0;
 }
