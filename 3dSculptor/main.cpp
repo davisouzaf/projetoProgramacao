1c0a7ebb55aec0d8356e66f2b27df551eddf67e7
davisouzaf <davi_sfernandes@hotmail.com>
2019-09-22 08:44:57 -0300

    terminado eu acho

--- 3dSculptor/main.cpp
+++ 3dSculptor/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
     Sculptor s(70,70,70);
-    s.setColor(1,1,0,1);
+    s.setColor(1,1,0,0.01);
     s.putSphere(0,70,0,15);
-    s.setColor(0,1,0,1);
+    s.setColor(0,1,0,0.5);
     s.putBox(0,50,0,25,0,25);
     s.cutBox(5,15,0,10,0,25);
     s.cutBox(35,45,0,10,0,25);
@@ -26,8 +26,10 @@ int main(){
     s.putBox(35,42,3,8,2,20);
     s.cutBox(0,8,13,20,0,25);
     s.cutSphere(48,13,15,5);
+    s.setColor(1,0,0,1);
     s.putEllipsoid(48,13,15,2,4,4);
     s.writeOFF("programateste.off");
+    s.~Sculptor();
     cout << "Done" << endl;
     return 0;
 
