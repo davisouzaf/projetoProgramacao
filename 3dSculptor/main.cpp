b27463228dd7e7b8aebe7ef9aab40a406eb806a9
davisouzaf <davi_sfernandes@hotmail.com>
2019-10-12 12:07:56 -0300

    comecei a parte de ler o arquivo de texto, mas falta pensar na criacao do vetor de figurageometrica

--- 3dSculptor/main.cpp
+++ 3dSculptor/main.cpp
@@ -1,22 +1,91 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
 #include "sculptor.h"
+#include "figurageometrica.h"
+#include "cutellipsoid.h"
+#include "putbox.h"
+#include "cutbox.h"
+#include "putellipsoid.h"
+#include "putvoxel.h"
+#include "cutvoxel.h"
+#include "putsphere.h"
+#include "cutsphere.h"
+
 using namespace std;
 
 int main(){
-    Sculptor s(50,50,150);
-    s.setColor(1,1,1,1);
-    s.putSphere(25,25,10,5);             //pomo, centro=(25,25,25)
+    string s, comando;
+    ifstream fin;
+    fin.open("/home/davi/Documentos/testeleitura.txt");
+    stringstream ss;
+    while(true){
+        getline(fin,s);
+        if(fin.good()){
+            stringstream ss(s);
+            ss>>comando;
+            cout<<comando<<endl;
+            //vector<*FiguraGeometrica*> f; como criar um vetor de objetos figurageometrica ???
+            if(comando.compare("dim")==0){
+                int nx,ny,nz;
+                ss>>nx>>ny>>nz;
 
-    //s.setColor(0,0,0,1);
-    s.putBox(24,27,24,27,10,35);         //cabo
+            }else if (comando.compare("putvoxel")==0){
+                int pos[3];
+                float color[4];
+                ss>>pos[0]>>pos[1]>>pos[2];
+                ss>>color[0]>>color[1]>>color[2]>>color[3];
+            }else if(comando.compare("cutvoxel")==0){
+                int pos[3];
+                ss>>pos[0]>>pos[1]>>pos[2];
+            }else if(comando.compare("putbox")==0){
+                int pos[6];
+                float color[4];
+                ss>>pos[0]>>pos[1]>>pos[2]>>pos[3]>>pos[4]>>pos[5];
+                ss>>color[0]>>color[1]>>color[2]>>color[3];
+            }else if (comando.compare("cutbox")==0) {
+                int pos[6];
+                ss>>pos[0]>>pos[1]>>pos[2]>>pos[3]>>pos[4]>>pos[5];
+            }else if (comando.compare("putsphere")==0) {
+                int pos[4];
+                int color[4];
+                ss>>pos[0]>>pos[1]>>pos[2]>>pos[3];
+                ss>>color[0]>>color[1]>>color[2]>>color[3];
+            }else if (comando.compare("cutsphere")==0) {
+                int pos[4];
+                ss>>pos[0]>>pos[1]>>pos[2]>>pos[3];
+            }else if (comando.compare("putellipsoid")==0) {
+                int pos[6];
+                int color[4];
+                ss>>pos[0]>>pos[1]>>pos[2]>>pos[3]>>pos[4]>>pos[5];
+                ss>>color[0]>>color[1]>>color[2]>>color[3];
+            }else if (comando.compare("cutellipsoid")==0) {
+                int pos[6];
+                ss>>pos[0]>>pos[1]>>pos[2]>>pos[3]>>pos[4]>>pos[5];
+            }
 
-    s.setColor(1,1,1,1);
+        }else {
+            break;
+        }
+    }
+
+
+    /*Sculptor s(50,50,150);             //pomo, centro=(25,25,25)
+    PutSphere ps(25,25,10,5,1,1,1,0.5);
+    ps.draw(s);
+
+    PutBox pb(24,27,24,27,10,35,0,0,0,0.745);//cabo
+    pb.draw(s);
+    s.setColor(1,1,1,0.8);
     s.putBox(10,40,22,29,35,42);         //guarda-mao
 
     s.cutBox(20,24,22,29,35,36);         //detalhe do guarda-mao 1
     s.cutBox(27,31,22,29,35,36);         //detalhe do guarda-mao 2
 
-    //s.setColor(0,0,0,1);
+    s.setColor(0,0,0,0.5);
     s.putBox(12,38,23,28,42,120);        //gume da espada
 
     s.cutBox(20,31,22,23,35,42);         //detalhe do guarda-mao 3
@@ -30,7 +99,7 @@ int main(){
     s.cutBox(23,28,27,28,37,42);         //detalhe entre o gume o guarda-mao no verso
     s.putBox(24,27,27,28,38,41);         //detalhe entre o gume o guarda-mao no verso 2
 
-    //s.setColor(0,0,0,1);
+    s.setColor(0,0,0,0.3);
     s.putBox(19,20,22,23,35,63);
     s.putBox(20,31,22,23,62,63);         //quadrado entre os circulos do gume
     s.putBox(31,32,22,23,35,63);
@@ -39,8 +108,9 @@ int main(){
     s.putBox(20,31,28,29,62,63);        //quadrado entre os circulos do gume no verso
     s.putBox(31,32,28,29,35,63);
 
-    s.writeOFF("testesword2.off");
-    cout << "Hello World!" << endl;
+    s.writeOFF("/home/davi/Documentos/testesword.off");
+    cout << "Done" << endl;*/
+
     return 0;
 
 }
